c5c4eccd81f7ac0411b968892803a8857195bb91
Carlos Perez <c.perezmendoza@uandresbello.edu>
2021-04-11 15:49:02 -0400

    1104211548

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello From V2
         </a>
       </header>
     </div>
