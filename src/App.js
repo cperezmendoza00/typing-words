fa10208aec70791a14c5e4aa79be75336ea07cb1
Carlos Perez <c.perezmendoza@uandresbello.edu>
2021-04-12 08:50:30 -0400

    Add backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello From V2
-        </a>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
