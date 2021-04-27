10191af176b2ab624faa1fe6150db07f02685aec
Carlos Perez <c.perezmendoza@uandresbello.edu>
2021-04-27 13:14:49 -0400

    Add credentials to git

---
+++ src/aws-credentials.js
@@ -0,0 +1,10 @@
+
+
+const awscredentials = {
+    region: "us-east-1",
+    accessKeyId: "AKIA223ERJJEBIIE43WH",
+    secretAccessKey: "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"
+};
+
+
+export default awscredentials;
