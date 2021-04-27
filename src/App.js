ea9ca5f2d9acccd72f671e9c9e31caaba7921cf2
Carlos Perez <c.perezmendoza@uandresbello.edu>
2021-04-27 12:46:07 -0400

    get voice

--- src/App.js
+++ src/App.js
@@ -1,99 +1,246 @@
-import logo from './logo.svg';
-
-
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listNotes } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
 
-const initialFormState = { name: '', description: '' }
+const AWS = require('aws-sdk')
 
+
+const Polly =  new AWS.Polly({
+  region: 'eu-west-1'
+})
+
+
+
+let study = [];
+let index = -1;
+let text_colors = [];
+let button_next = 'Start'
+
+let sound_ok = new Audio('/success.wav');
+sound_ok.volume = 1; 
+
+let sound_error = new Audio('/error.flac');
+sound_error.volume = 0.1; 
+
+let words = require('./words/wordsbylevel.json');
+let configs = require('./words/config.json')    
+let i = 0;
+
+words.map(level=>{
+  let study_by_level = [];
+  while(study_by_level.length < configs[i]) {
+    let word_index = Math.floor((Math.random() * (level.length - 0) + 0));
+    study_by_level.push(level[word_index])
+    study.push(level[word_index])    
+  }
+  //study.push(study_by_level)
+  i++;
+})
+
+
+const initialFormState = { text: ''}
 function App() {
 
-  async function onChange(e) {
-    if (!e.target.files[0]) return
-    const file = e.target.files[0];
-    setFormData({ ...formData, image: file.name });
-    await Storage.put(file.name, file);
-    fetchNotes();
-  }
-
+  const [current_word, setCurrentWord] = useState({word:'Empezar'});
+  const [last_words, setLastWords] = useState([]);
+  const [formData, setFormData] = useState( initialFormState);
+  const [current_audio, setcurrent_audio] = useState(null);
   
-  const [notes, setNotes] = useState([]);
-  const [formData, setFormData] = useState(initialFormState);
+  
+  function play(){
+    if(current_audio !== null) {
+      current_audio.play();
+    }    
+  }
 
-  useEffect(() => {
-    fetchNotes();
-  }, []);
+  async function getaudio(text){
+    var params = {
+      OutputFormat: "mp3", 
+      SampleRate: "24000", 
+      Text: text,
+      TextType: "text", 
+      VoiceId: "Joanna"
+    };
+    var signer = new AWS.Polly.Presigner(params, Polly)
+    return await signer.getSynthesizeSpeechUrl(params);
+  }
 
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(notesFromAPI.map(async note => {
-      if (note.image) {
-        const image = await Storage.get(note.image);
-        note.image = image;
+  function next(){
+    
+    if(index >=0){
+      last_words.unshift(study[index])
+      setLastWords(last_words);
+      if(typeof current_word.errors == "undefined" ) {
+        current_word.errors = 1;
       }
-      return note;
-    }))
-    setNotes(apiData.data.listNotes.items);
-  }
 
-  async function createNote() {
-    if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-    if (formData.image) {
-      const image = await Storage.get(formData.image);
-      formData.image = image;
+    } else {
+      button_next = 'Play again'
     }
-    setNotes([ ...notes, formData ]);
-    setFormData(initialFormState);
+    index++;
+    
+    getaudio(study[index].word).then(url=>{
+      let audio = new Audio(url);
+      audio.play()
+      
+      setcurrent_audio(audio)
+      setCurrentWord(study[index]);
+      setFormData(initialFormState);
+    })
   }
 
-  async function deleteNote({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  function validate(e){
+    text_colors = [];
+
+    if(typeof current_word.errors == "undefined" ) {
+      current_word.errors = 0;
+    }
+
+    let error = false;
+
+
+    for(let i = 0; i< e.target.value.length; i++){
+      let letter = e.target.value[i];
+      if(current_word.word[i] == e.target.value[i]) {
+        text_colors.push({ok:true,letter:letter==' ' ? '_':letter})
+      } else {
+        text_colors.push({ok:false,letter:letter==' ' ? '_':letter})
+        sound_error.play()
+        error = true;
+          
+      }
+    }
+
+    if(error) current_word.errors++;
+
+
+    if(e.target.value == current_word.word ) {
+      sound_ok.play()
+      setTimeout(()=>{
+        next()
+      },200)
+      text_colors = []
+    }
+    setFormData({ ...formData, 'text': e.target.value})
   }
+
+
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
-        value={formData.description}
-      />
-      <input
-        type="file"
-        onChange={onChange}
-      />
-      <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
-        {
-          notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteNote(note)}>Delete note</button>
-              {
-                note.image && <img src={note.image} style={{width: 400}} />
+      <AmplifySignOut />
+      <h1>Write what you hear</h1>
+  
+      { current_audio !== null 
+        &&
+        <button className="buttons" onClick={play}>{button_next}</button>
+      }
+      <div className="text_container">
+        { current_audio !== null &&
+
+          <input className="write"
+            onChange={validate}
+            
+            value={formData.text}
+          />
+        }
+        { current_audio !== null &&
+          <div className="write">{
+            text_colors.map((letter,index) => (
+              <label key={letter.letter + index}>
+              { 
+                letter.ok && <label className="letter_green">{letter.letter}</label> 
               }
-            </div>
-          ))
+              { 
+                !letter.ok && <label className="letter_red">{letter.letter}</label> 
+              }
+              </label>
+            ))
+          }</div>
         }
       </div>
-      <AmplifySignOut />
+      <button className="buttons" onClick={next}>Next word</button>
+      { last_words.length > 0 &&
+        <div className="table_words">
+          <div className="table_titles">
+              <div>Word <label className="belong">(Level)</label></div>
+              <div>Errors</div>
+            </div>
+          <div className="results_table_words">
+          {
+            last_words.map((last_word,index) => (
+              <div key={last_word.word + index}>
+                <div>{last_word.word} <label className="belong">({last_word.belongto})</label></div>
+                <div>{last_word.errors}</div>
+              </div>
+            ))
+          }
+          </div>
+        </div>
+      }
     </div>
   );
+}
+
+function nametoindex(name,type){
+  let index = null;
+  if(type == 'belong'){
+    if(name == "a1") index = 0;
+    if(name == "a2") index = 1;
+    if(name == "b1") index = 2;
+    if(name == "b2") index = 3;
+    if(name == "c1") index = 4;
+  }
+  if(type == 'desc'){
+    if(name == "adverb") index = 1;
+    if(name == "auxiliary verb") index = 2;
+    if(name == "conjunction") index = 3;
+    if(name == "definite article") index = 4;
+    if(name == "determiner") index = 5;
+    if(name == "exclamation") index = 6;
+    if(name == "indefinite article") index = 7;
+    if(name == "infinitive marker") index = 8;
+    if(name == "linking verb") index = 9;
+    if(name == "modal verb") index = 10;
+    if(name == "noun") index = 11;
+    if(name == "number") index = 12;
+    if(name == "ordinal number") index = 13;
+    if(name == "preposition") index = 14;
+    if(name == "pronoun") index = 15;
+    if(name == "verb") index = 16;
+  }
+  return index
+}
+
+function indextoname(index,type){
+  let name = null;
+  if(type == 'belong'){
+    if(index == 0) name = "a1";
+    if(index == 1) name = "a2";
+    if(index == 2) name = "b1";
+    if(index == 3) name = "b2";
+    if(index == 4) name = "c1";
+  }
+  if(type == 'desc'){
+    if(index == 0) name ="adverb";
+    if(index == 1) name = "auxiliary verb";
+    if(index == 2) name = "conjunction";
+    if(index == 3) name = "definite article";
+    if(index == 4) name = "determiner";
+    if(index == 5) name = "exclamation";
+    if(index == 6) name = "indefinite article";
+    if(index == 7) name = "infinitive marker";
+    if(index == 8) name = "linking verb";
+    if(index == 9) name = "modal verb";
+    if(index == 10) name = "noun";
+    if(index == 11) name = "number";
+    if(index == 12) name = "ordinal number";
+    if(index == 13) name = "preposition";
+    if(index == 14) name = "pronoun";
+    if(index == 15) name = "verb";
+  }
+  return name
 }
 
 export default withAuthenticator(App);
---
+++ src/backup/01-App.js
@@ -0,0 +1,224 @@
+import logo from './logo.svg';
+
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { API, Storage, Auth} from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+
+
+const initialFormState = { name: '', description: '' }
+
+function App() {
+
+  function nametoindex(name,type){
+    let index = null;
+    if(type == 'belong'){
+      if(name == "a1") index = 0;
+      if(name == "a2") index = 1;
+      if(name == "b1") index = 2;
+      if(name == "b2") index = 3;
+      if(name == "c1") index = 4;
+    }
+    if(type == 'desc'){
+      if(name == "adverb") index = 1;
+      if(name == "auxiliary verb") index = 2;
+      if(name == "conjunction") index = 3;
+      if(name == "definite article") index = 4;
+      if(name == "determiner") index = 5;
+      if(name == "exclamation") index = 6;
+      if(name == "indefinite article") index = 7;
+      if(name == "infinitive marker") index = 8;
+      if(name == "linking verb") index = 9;
+      if(name == "modal verb") index = 10;
+      if(name == "noun") index = 11;
+      if(name == "number") index = 12;
+      if(name == "ordinal number") index = 13;
+      if(name == "preposition") index = 14;
+      if(name == "pronoun") index = 15;
+      if(name == "verb") index = 16;
+    }
+    return index
+  }
+
+  function indextoname(index,type){
+    let name = null;
+    if(type == 'belong'){
+      if(index == 0) name = "a1";
+      if(index == 1) name = "a2";
+      if(index == 2) name = "b1";
+      if(index == 3) name = "b2";
+      if(index == 4) name = "c1";
+    }
+    if(type == 'desc'){
+      if(index == 0) name ="adverb";
+      if(index == 1) name = "auxiliary verb";
+      if(index == 2) name = "conjunction";
+      if(index == 3) name = "definite article";
+      if(index == 4) name = "determiner";
+      if(index == 5) name = "exclamation";
+      if(index == 6) name = "indefinite article";
+      if(index == 7) name = "infinitive marker";
+      if(index == 8) name = "linking verb";
+      if(index == 9) name = "modal verb";
+      if(index == 10) name = "noun";
+      if(index == 11) name = "number";
+      if(index == 12) name = "ordinal number";
+      if(index == 13) name = "preposition";
+      if(index == 14) name = "pronoun";
+      if(index == 15) name = "verb";
+    }
+    return name
+  }
+  async function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
+  }
+
+  
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ 
+      query: listNotes,
+    });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+      return note;
+    }))
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function example() {
+
+    let words = require('./words/wordsbylevel.json');
+    let configs = require('./words/config.json')    
+    let i = 0;
+
+    let study = [];
+    words.map(level=>{
+      console.log(indextoname(i,'belong'))
+      console.log(level)
+
+      console.log('buscar:',configs[i],'%');
+      let word_index = Math.random() * (level.length - 0) + 0;
+      i++;
+    })
+
+    
+    console.log(configs)
+
+    /*configs.map((config,index)=>{
+      //console.log(indextoname(index,'belong'))
+      ////console.log(config)
+      let buscar = true;
+
+      let i = 0;
+      while(buscar){
+        //console.log(i)
+
+        //Math.random() * (max - min) + min;
+
+
+        if(i==100){
+          buscar = false;
+        }
+        i++;
+      }
+    })*/
+
+    try{
+      let userinfo = await Auth.currentSession();
+    } catch {
+      
+    }
+    
+    //console.log(userinfo)
+    
+  }
+  async function createNote() {
+
+    let loggedin = false;
+    await Auth.currentSession()
+    .then(r=>{
+      loggedin = true;
+    })
+    .catch(e=>{
+      loggedin = false;
+    })
+
+    console.log(loggedin);
+    if(!loggedin){
+      //redirect
+    }
+
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
+  return (
+    <div className="App">
+      <h1>My Notes App</h1>
+
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <input
+        type="file"
+        onChange={onChange}
+      />
+      <button onClick={example}>Example</button>
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+              {
+                note.image && <img src={note.image} style={{width: 400}} />
+              }
+            </div>
+          ))
+        }
+      </div>
+      <AmplifySignOut />
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
